b0c009d745ecb473437f93d49a93d5e20da05ed9
batout-asma <benramoulasma@gmail.com>
2025-02-17 23:28:11 +0100

    add useritem and userlist

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,6 +1,16 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 const Users = () => {
-  return <h2>Users work!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Batout",
+      image:
+        "https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
