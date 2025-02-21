c49869251a878934f3d2837850aed28e33536726
batout-asma <benramoulasma@gmail.com>
2025-02-21 15:20:48 +0100

    fixed the ReactDom to createRoot and delete the corrupted logos

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -11,6 +11,13 @@ const Users = () => {
         "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
       places: 3,
     },
+    {
+      id: "u2",
+      name: "Mchimch",
+      image:
+        "https://images.unsplash.com/photo-1440589473619-3cde28941638?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      places: 1,
+    },
   ];
 
   return <UsersList items={USERS} />;
