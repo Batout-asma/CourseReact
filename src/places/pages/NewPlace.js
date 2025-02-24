5901315c76f27178cf040269050a280b61c2ae0a
batout-asma <benramoulasma@gmail.com>
2025-02-25 00:57:12 +0100

    input in the new places page

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlace.css";
+
 const NewPlace = () => {
-  return <form></form>;
+  return (
+    <form className="place-form">
+      <Input id="title" label="Title" element="input" type="text" />
+    </form>
+  );
 };
 
 export default NewPlace;
