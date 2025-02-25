9876edf447f58a32065fe813e38d2bc66d9adccb
batout-asma <benramoulasma@gmail.com>
2025-02-25 23:31:17 +0100

    validation on the imput and also on touch check

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,12 +1,20 @@
 import React from "react";
 
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 import Input from "../../shared/components/FormElements/Input";
 import "./NewPlace.css";
 
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input id="title" label="Title" element="input" type="text" />
+      <Input
+        id="title"
+        label="Title"
+        element="input"
+        type="text"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+      />
     </form>
   );
 };
