e7c9bed3a5eb8d7417b65bf8adff150b9329a591
batout-asma <benramoulasma@gmail.com>
2025-02-27 00:28:28 +0100

    update place and share style file

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -6,7 +6,8 @@ import {
 } from "../../shared/util/validators";
 import Button from "../../shared/components/FormElements/Button";
 import Input from "../../shared/components/FormElements/Input";
-import "./NewPlace.css";
+
+import "./PlaceForm.css";
 
 const formReducer = (state, action) => {
   switch (action.type) {
@@ -40,6 +41,10 @@ const NewPlace = () => {
         isValid: false,
       },
       description: {
+        value: "",
+        isValid: false,
+      },
+      address: {
         value: "",
         isValid: false,
       },
